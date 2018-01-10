e4d80ef3d3b3789cc2caada8c4eec482a4121ade
邓成 <dengcheng@qfpay.com>
2018-01-10 10:19:47 +0800

    add

---
+++ controllers/password.go
@@ -0,0 +1,63 @@
+package controllers
+
+
+import(
+	"log"
+	"time"
+	"math/rand"
+	"crypto/sha1"
+	"encoding/hex"
+	"strings"
+)
+
+const (
+	KC_RAND_KIND_NUM   = 0	// 纯数字
+	KC_RAND_KIND_LOWER = 1	// 小写字母
+	KC_RAND_KIND_UPPER = 2	// 大写字母
+	KC_RAND_KIND_ALL   = 3 	// 数字、大小写字母
+)
+
+// 随机字符串
+func RandStr(size int, kind int) []byte {
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	rand.Seed(time.Now().UnixNano())
+	for i :=0; i < size; i++ {
+		if is_all { // random ikind
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base+rand.Intn(scope))
+	}
+	return result
+}
+
+func GenPassword(md5Password string) string {
+	result := RandStr(5, KC_RAND_KIND_ALL)
+	randomStr := string(result)
+	log.Println(randomStr)
+	newStr := randomStr + md5Password
+	log.Println(newStr)
+    r := sha1.Sum([]byte(newStr))
+	sha1Hex := hex.EncodeToString(r[:])
+	log.Println(sha1Hex)
+	//return string(sha1Hex)
+	return "sha1" + "$" + randomStr + "$" + string(sha1Hex)
+}
+
+func CheckPassword(fullPassword string, md5Password string) bool {
+	log.Printf("full password=%s", fullPassword)
+	all := strings.Split(fullPassword, "$")
+	salt := all[1]
+	password := all[2]
+	log.Printf("salt=%s|password=%s", salt, password)
+	newStr := salt + md5Password
+	log.Printf("new str=%s", newStr)
+	r := sha1.Sum([]byte(newStr))
+	sha1Hex := hex.EncodeToString(r[:])
+	log.Println(sha1Hex)
+    if string(sha1Hex) != password {
+    	return false
+	}
+	return true
+}
---
+++ model/model.go
@@ -0,0 +1,63 @@
+package model
+
+import (
+	"log"
+)
+
+type Response struct {
+	Respcd 	string	`json:"respcd"`
+	Resperr string	`json:"resperr"`
+	Respmsg string	`json:"respmsg"`
+	Data    interface{} `json:"data"`
+}
+
+
+//struct定义的属性是小写开头的，不是public的，这样是不能跨包调用
+type User struct {
+    Id    			int64	`json:"id"`
+    Login_name		string	`json:"login_name"`
+    Nick_name		string	`json:"nick_name"`
+    Phone_num		string	`json:"phone_num"`
+    Password		string	`json:"password"`
+    User_type		int64	`json:"user_type"`
+    User_role       int64   `json:"user_role"`
+    Email           string	`json:"email"`
+    State           int64	`json:"state"`
+    Username		string	`json:"username"`
+    Ctime           string  `json:"ctime"`
+}
+
+type Channel struct {
+	Id				int64   `json:"id"`
+	Userid          int64   `json:"userid"`
+	Remain_times    int64   `json:"remain_times"`
+	Training_amt_per  int64  `json:"training_amt_per"`
+	Divide_percent  float64 `json:"divide_percent"`
+	Status          int64    `json:"status"`
+	Is_valid        int64   `json:"is_valid"`
+	Is_prepayment   int64    `json:"is_prepayment"`
+	Ctime           string  `json:"ctime"`
+	Utime           string  `json:"utime"`
+	Channel_name    string  `json:"channel_name"`
+
+}
+
+type Rule struct {
+	Id    			int64     `json:"id"`
+	Name            string    `json:"name"`
+	TotalAmt        int64     `json:"total_amt"`
+    TrainingTimes   int64     `json:"training_times"`
+    Description     string    `json:"description"`
+    CTime           string    `json:"ctime"`
+    UTime           string    `json:"utime"`
+    IsValid         int64     `json:"is_valid"`
+    Status          string    `json:"state"`
+}
+
+func (u User) Valid() bool {
+	log.Printf("user id=%d", u.Id)
+	if u.Id != 0 {
+		return true
+	}
+	return false
+}
