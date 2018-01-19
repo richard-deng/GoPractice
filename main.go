9f9662f0cdbaf44a3ad65a0b9c8c53edcfd22772
邓成 <dengcheng@qfpay.com>
2018-01-19 17:13:17 +0800

    rename

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func logginHandler(next http.Handler) http.Handler {
+func logHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		start := time.Now()
 		log.Printf("Started %s %s", r.Method, r.URL.Path)
@@ -28,21 +28,21 @@ func main() {
 	mux.Handle("/", fs)
 
 	//页面
-	mux.Handle("/login.html", logginHandler(http.HandlerFunc(controllers.Login)))
-	mux.Handle("/overview.html", logginHandler(http.HandlerFunc(controllers.Overview)))
-	mux.Handle("/users.html", logginHandler(http.HandlerFunc(controllers.UserView)))
-	mux.Handle("/channel.html", logginHandler(http.HandlerFunc(controllers.ChannelView)))
-	mux.Handle("/rule.html", logginHandler(http.HandlerFunc(controllers.RuleView)))
+	mux.Handle("/login.html", logHandler(http.HandlerFunc(controllers.Login)))
+	mux.Handle("/overview.html", logHandler(http.HandlerFunc(controllers.Overview)))
+	mux.Handle("/users.html", logHandler(http.HandlerFunc(controllers.UserView)))
+	mux.Handle("/channel.html", logHandler(http.HandlerFunc(controllers.ChannelView)))
+	mux.Handle("/rule.html", logHandler(http.HandlerFunc(controllers.RuleView)))
 
 	//接口数据
-	mux.Handle("/api/user/all", logginHandler(http.HandlerFunc(controllers.UserHandler)))
-	mux.Handle("/api/user/mobile", logginHandler(http.HandlerFunc(controllers.UserInfoByPhoneNumber)))
-	mux.Handle("/api/user/password/change", logginHandler(http.HandlerFunc(controllers.UserChangePasswordHandler)))
-	mux.Handle("/api/channel/all", logginHandler(http.HandlerFunc(controllers.ChannelHandler)))
-	mux.Handle("/api/channel/names/all", logginHandler(http.HandlerFunc(controllers.ChannelNamesHandler)))
-	mux.Handle("/api/rule/all", logginHandler(http.HandlerFunc(controllers.RuleHandler)))
-	mux.Handle("/api/rule/names/all", logginHandler(http.HandlerFunc(controllers.RuleNamesHandler)))
-	mux.Handle("/api/rule/create", logginHandler(http.HandlerFunc(controllers.RuleCreateHandler)))
+	mux.Handle("/api/user/all", logHandler(http.HandlerFunc(controllers.UserHandler)))
+	mux.Handle("/api/user/mobile", logHandler(http.HandlerFunc(controllers.UserInfoByPhoneNumber)))
+	mux.Handle("/api/user/password/change", logHandler(http.HandlerFunc(controllers.UserChangePasswordHandler)))
+	mux.Handle("/api/channel/all", logHandler(http.HandlerFunc(controllers.ChannelHandler)))
+	mux.Handle("/api/channel/names/all", logHandler(http.HandlerFunc(controllers.ChannelNamesHandler)))
+	mux.Handle("/api/rule/all", logHandler(http.HandlerFunc(controllers.RuleHandler)))
+	mux.Handle("/api/rule/names/all", logHandler(http.HandlerFunc(controllers.RuleNamesHandler)))
+	mux.Handle("/api/rule/create", logHandler(http.HandlerFunc(controllers.RuleCreateHandler)))
 
 	//启动服务
 	log.Println("Listening :8080")
